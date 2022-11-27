492bc7eb8f11a109ef1bb99e523a718f81618f56
CoffeeSlurpr <petobalint09@gmail.com>
2022-11-27 19:11:38 +0100

    added context for handling API access token

---
+++ src/context/TokenContext.jsx
@@ -0,0 +1,48 @@
+import React, { useState, createContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+const TokenContext = createContext();
+
+export const TokenProvider = ({ children }) => {
+  const [token, setToken] = useState({});
+  let location = useLocation();
+
+  const refreshToken = () => {
+    //refreshes token if expired
+    if (!token.expires || token.expires - new Date().getTime() < 1) {
+      fetchToken();
+    }
+  };
+
+  const fetchToken = () => {
+    axios
+      .get("http://localhost:3001/fetchToken")
+      .then((res) => {
+        setToken({
+          token: res.data.access_token,
+          tokenType: res.data.token_type,
+          expires: new Date().getTime() + res.data.expires_in * 1000,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchToken();
+  }, []);
+
+  useEffect(() => {
+    refreshToken();
+  }, [location]);
+
+  console.log(token);
+
+  return (
+    <TokenContext.Provider value={{ token }}>{children}</TokenContext.Provider>
+  );
+};
+
+export default TokenContext;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { TokenProvider } from "./context/TokenContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <TokenProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </TokenProvider>
+  </BrowserRouter>
 );
